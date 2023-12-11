b3367b4b4041764c3d5958442073221eef526414
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-11 11:38:16 +0100

    footer

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,19 @@
 import { token } from "./token.js";
-
 console.log(token);
+
+const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem";
+const options = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": token,
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+} catch (error) {
+    console.error(error);
+}
