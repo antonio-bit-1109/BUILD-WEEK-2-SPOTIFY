0f51d067a1ff049332a9abadee5adb4c2b6cc0d8
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-16 11:48:31 +0100

    GG

--- assets/js/map-fetch.js
+++ assets/js/map-fetch.js
@@ -1,13 +1,4 @@
-/* const nums = [13, 50, 32, 53];
-
-const promises = nums.map((n) =>
-    fetch("https://striveschool-api.herokuapp.com/api/deezer/artist/" + n).then((resp) => resp.json())
-);
-
-Promise.all(promises)
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
- */
+/* REFACTOR CHIAMATE ARTIST FETCH() */
 
 import { token } from "./token.js";
 
@@ -21,24 +12,77 @@ const options = {
 
 window.addEventListener("load", () => {
     sayHiProperly();
-    callApiArtists();
-});
+    const arrNumRandom = callApiArtists();
 
+    const promises = arrNumRandom.map((num) => {
+        const URL = `https://deezerdevs-deezer.p.rapidapi.com/artist/${num}`;
+
+        return fetch(URL, options).then((response) => {
+            /* condizioni di guardia */
+            if (!response.ok) {
+                if (response.status > 400 && response.status < 500) {
+                    if (response.status === 429) {
+                        throw new Error("ME STAI A CHIAMA TROPPO ZII, NUN T'ACCOLLA", response.status);
+                    } else {
+                        throw new Error("errore bad request, MEA CULPA", response.status);
+                    }
+                }
+                if (response.status > 500 && response.status < 600) {
+                    throw new Error("FORSE FORSE QUALQUADRA NON COSA DELLà", response.status);
+                }
+            } else {
+                /* risposta positiva */
+                return response.json();
+            }
+        });
+    });
+
+    Promise.all(promises)
+        .then((datas) => {
+            /* objects from 6 fetch from API artists */
+            console.log(datas);
+            const nomiArtisti = getAutorsNames(datas);
+            PushNamesIntoLists(nomiArtisti);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+/* DEFINIZ FUNZIONI  */
+
+const getAutorsNames = (datas) => {
+    /* data è il nome che io do ad ogni elemento dentro datas */
+    const arrNamesArtists = datas.map((data) => data.name);
+    return arrNamesArtists;
+};
+
+const PushNamesIntoLists = (nomiArtisti) => {
+    console.log(nomiArtisti);
+
+    for (let i = 0; i < nomiArtisti.length; i++) {
+        let singleName = nomiArtisti[i];
+        getPlaylists(singleName, "buonPomeriggio", "small");
+        getPlaylists(singleName, "ascoltatiDiRecente", "large");
+        getPlaylists(singleName, "iTuoiMix", "large");
+        getPlaylists(singleName, "tendenze", "large");
+        getPlaylists(singleName, "popolare", "large");
+    }
+};
 const callApiArtists = () => {
     const arrRandomIds = [];
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
         let randomNumber;
         do {
             randomNumber = Math.floor(Math.random() * 400);
         } while (arrRandomIds.includes(randomNumber));
 
         arrRandomIds.push(randomNumber);
+        console.log(arrRandomIds);
     }
     return arrRandomIds;
 };
 
-/* DEFINIZ FUNZIONI  */
 const sayHiProperly = () => {
     const h2 = document.getElementById("sayHello");
     let timeOfTheDay = new Date();
@@ -52,5 +96,92 @@ const sayHiProperly = () => {
         h2.innerHTML = "Buon Pomeriggio!";
     } else {
         h2.innerHTML = "Buonasera!";
+    }
+};
+
+function getPlaylists(value, container, cardType) {
+    const apiUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + value;
+
+    fetch(apiUrl, options)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Errore nella richiesta: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            generateCardList(data, container, cardType);
+            console.log("Lista di playlist:", data);
+        })
+        .catch((error) => {
+            console.error("Si è verificato un errore:", error);
+        });
+}
+
+const generateCardList = (arrayObj, container, cardType) => {
+    const cardContainer = document.getElementById(container);
+    cardContainer.innerHTML = "";
+    for (let i = 0; i < 6; i++) {
+        let card = createCard(arrayObj.data[i], cardType);
+        cardContainer.appendChild(card);
+    }
+};
+
+const createCard = (obj, cardType) => {
+    if (cardType == "small") {
+        const card = document.createElement("div");
+        card.classList.add("col-12", "col-md-6", "col-xl-4");
+        card.innerHTML = `
+
+        <a href="./albumpage.html?idAlbum=${obj.album.id}"><div class="card m-2 bg-dark text-white">
+    <div class="row g-0">
+      <div class="col-2">
+        <img
+          src="${obj.album.cover}"
+          class="small-card"
+          alt="card"
+        />
+        
+
+      </div>
+      <div class="col-10">
+        <div class="card-body d-flex justify-content-center align-items-center h-100 w-80">
+          <h6 class="card-title m-0">${obj.album.title}</h6>
+        </div>
+      </div>
+    </div>
+  </div></a>
+    `;
+        return card;
+    } else if (cardType == "large") {
+        const card = document.createElement("div");
+        card.classList.add("col-12", "col-md-6", "col-xl-4", "col-xxl-2");
+        card.innerHTML = `
+        
+        <a href="./albumpage.html?idAlbum=${obj.album.id}">
+        <div class="card customCard bg-black border-0 bg-opacity-50 m-2">
+            <div class="d-flex justify-content-center align-item-center position-relative">
+                <img
+                    src="${obj.album.cover}"
+                    class="card-img-top max-h-180 max-w-180 object-fit-cover mx-2 mt-2 rounded"
+                    alt="Album cover"
+                />
+                <img src="./assets/imgs/play-fill.svg" class="position-absolute positionCustom" />
+            </div>
+            <div class="card-body fix-h-100">
+                <h6 class="card-title overflowCustom max-h-50 fs-6">
+                    <a class="customColorA" href="./albumpage.html?idAlbum=${obj.album.id}">${obj.album.title}</a>
+                </h6>
+                <p class="card-text fs-8">
+                    <a class="customColorA" href="./artistpage.html?idArtist=${obj.artist.id}&idAlbum=${obj.album.id}"
+                        >${obj.artist.name}</a
+                    >
+                </p>
+            </div>
+        </div></a
+    >
+
+  `;
+        return card;
     }
 };
